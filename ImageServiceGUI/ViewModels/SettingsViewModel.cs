240d8774e0777e9622f59f9cea08c84595737744
Eden Yefet <edenyefet@gmail.com>
2018-05-28 15:11:07 +0300

    Reformatted SettingsViewModel into SettingsViewModel and SettingsModel.

--- ImageServiceGUI/ViewModels/SettingsViewModel.cs
+++ ImageServiceGUI/ViewModels/SettingsViewModel.cs
@@ -1,98 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Windows;
-using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Threading;
-using Infrastructure;
-using Infrastructure.Enums;
+using ImageServiceGUI.Model;
 using Infrastructure.Event;
 using Microsoft.Practices.Prism.Commands;
 
 namespace ImageServiceGUI.ViewModels
 {
-    internal class SettingsViewModel : INotifyPropertyChanged
+    internal class SettingsViewModel : ViewModel
     {
-        private const string WaitingForConnection = "Waiting for connection...";
+        private readonly ISettingsModel _model;
         private readonly Dispatcher _uiDispatcher;
-        private string _logName = WaitingForConnection;
-        private string _outputDirectory = WaitingForConnection;
-
         private string _selectedDirectoryHandler;
-        private string _sourceName = WaitingForConnection;
-        private int _thumbnailSize;
 
         /// <summary>
         ///     Initializes a new instance of the <see cref="SettingsViewModel" /> class.
         /// </summary>
         public SettingsViewModel()
         {
-            _uiDispatcher = Application.Current.Dispatcher;
             Debug.WriteLine("SettingsViewModel c'tor");
-
-            DirectoryHandlers = new ObservableCollection<string>();
-            BindingOperations.EnableCollectionSynchronization(DirectoryHandlers, DirectoryHandlers);
             _uiDispatcher = Application.Current.Dispatcher;
-            BackgroundColor = new SolidColorBrush(Colors.SlateGray);
-            GuiTcpClientSingleton.Instance.ConnectedToService += OnClientConnectedToService;
+            _model = new SettingsModel(_uiDispatcher);
             GuiTcpClientSingleton.Instance.DirectoryHandlerRemoved += OnDirectoryHandlerSuccessfulyRemoved;
-            SubmitRemove = new DelegateCommand<object>(OnRemove, CanRemove);
+            SubmitRemoveCommand = new DelegateCommand<object>(OnRemove, CanRemove);
             PropertyChanged += RemoveSelectedHandler;
-
-            GuiTcpClientSingleton.Instance.ConfigurationReceived += OnSettingsInfoReceived;
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public ObservableCollection<string> DirectoryHandlers { get; }
-
-        public SolidColorBrush BackgroundColor { get; set; }
-
-        public string OutputDirectory
-        {
-            get => _outputDirectory;
-            set
+            _model.PropertyChanged += delegate(object sender, PropertyChangedEventArgs args)
             {
-                _outputDirectory = value;
-                NotifyPropertyChanged("OutputDirectory");
-            }
+                NotifyPropertyChanged(args.PropertyName);
+            };
         }
 
-        public int ThumbnailSize
+        public ObservableCollection<string> DirectoryHandlers
         {
-            get => _thumbnailSize;
-            set
-            {
-                _thumbnailSize = value;
-                NotifyPropertyChanged("ThumbnailSize");
-            }
+            get => _model.DirectoryHandlers;
+            set => _model.DirectoryHandlers = value;
         }
 
-        public string LogName
-        {
-            get => _logName;
-            set
-            {
-                _logName = value;
-                NotifyPropertyChanged("LogName");
-            }
-        }
+        public SolidColorBrush BackgroundColor => _model.BackgroundColor;
 
-        public string SourceName
-        {
-            get => _sourceName;
-            set
-            {
-                _sourceName = value;
-                NotifyPropertyChanged("SourceName");
-            }
-        }
+        public string OutputDirectory => _model.OutputDirectory;
 
-        public ICommand SubmitRemove { get; }
+        public int ThumbnailSize => _model.ThumbnailSize;
+
+        public string LogName => _model.LogName;
+
+        public string SourceName => _model.SourceName;
+
+        public ICommand SubmitRemoveCommand { get; }
 
         public string SelectedDirectoryHandler
         {
@@ -104,53 +63,6 @@ namespace ImageServiceGUI.ViewModels
             }
         }
 
-        /// <summary>
-        ///     Called when a client is connected to service.
-        /// </summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
-        private void OnClientConnectedToService(object sender, EventArgs e)
-        {
-            Debug.WriteLine("In SettingsViewModel->OnClientConnectedToService");
-            _uiDispatcher.BeginInvoke(new Action(() =>
-            {
-                BackgroundColor = new SolidColorBrush(Colors.DarkCyan);
-                NotifyPropertyChanged("BackgroundColor");
-            }));
-        }
-
-        public void NotifyPropertyChanged(string name)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
-
-
-        /// <summary>
-        ///     Called when the settings information is received.
-        /// </summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="eventArgs">The <see cref="ConfigurationReceivedEventArgs" /> instance containing the event data.</param>
-        public void OnSettingsInfoReceived(object sender, ConfigurationReceivedEventArgs eventArgs)
-        {
-            Debug.WriteLine("In OnSettingsInfoReceived");
-            _uiDispatcher.BeginInvoke(new Action(() => { SetSettingsInfo(SettingsInfo.FromJson(eventArgs.Args)); }));
-        }
-
-        /// <summary>
-        ///     Sets the settings information.
-        /// </summary>
-        /// <param name="settingsInfo">The settings information.</param>
-        private void SetSettingsInfo(SettingsInfo settingsInfo)
-        {
-            LogName = settingsInfo.LogName;
-            SourceName = settingsInfo.SourceName;
-            OutputDirectory = settingsInfo.OutputDirectory;
-            ThumbnailSize = settingsInfo.ThumbnailSize;
-
-            List<string> handlers = settingsInfo.HandledDirectories;
-
-            foreach (string handler in handlers) DirectoryHandlers.Add(handler);
-        }
 
         /// <summary>
         ///     Called when a directory handler is successfuly removed.
@@ -176,7 +88,7 @@ namespace ImageServiceGUI.ViewModels
         /// <param name="args">The <see cref="PropertyChangedEventArgs" /> instance containing the event data.</param>
         public void RemoveSelectedHandler(object sender, PropertyChangedEventArgs args)
         {
-            DelegateCommand<object> command = SubmitRemove as DelegateCommand<object>;
+            DelegateCommand<object> command = SubmitRemoveCommand as DelegateCommand<object>;
             command?.RaiseCanExecuteChanged();
         }
 
@@ -187,11 +99,9 @@ namespace ImageServiceGUI.ViewModels
         ///     Note: This method doesn't remove the dir handler from the ListBox.
         /// </summary>
         /// <param name="obj">The object.</param>
-        private void OnRemove(object obj)
+        public void OnRemove(object obj)
         {
-            Debug.WriteLine("In OnRemove");
-            string command = (int) CommandEnum.CloseDirectoryHandlerCommand + "|" + SelectedDirectoryHandler;
-            GuiTcpClientSingleton.Instance.Writer.Write(command);
+            _model.OnRemove(SelectedDirectoryHandler);
         }
 
         /// <summary>
