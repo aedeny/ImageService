2c7985f8c7f869386f5ddaca03462310d1cd158a
Eden Yefet <edenyefet@gmail.com>
2018-06-02 19:28:33 +0300

    Created web skeleton.

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Config()
+        {
+            ViewBag.Message = "This is the configuration page.";
+            return View();
+        }
+
+        public ActionResult Photos()
+        {
+            ViewBag.Message = "All your selfies and other photos are here.";
+
+            return View();
+        }
+
+        public ActionResult Logs()
+        {
+            ViewBag.Message = "Useless log entries that no one cares about.";
+
+            return View();
+        }
+    }
+}
