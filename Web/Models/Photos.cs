3cb157db04f66b674ee52effb19df150730db00d
Eden Yefet <edenyefet@gmail.com>
2018-06-05 17:01:25 +0300

    Removed redundant code.

---
+++ Web/Models/Photos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class Photos
+    {
+        [DataType(DataType.Text)]
+        [Display(Name = "Thumbnails")]
+        public ObservableCollection<string> Thumbnails { get; set; }
+    }
+}
