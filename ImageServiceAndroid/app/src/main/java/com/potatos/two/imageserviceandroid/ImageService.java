a4c158aa6105c486dc2c5c3f9766086977428923
Eden Yefet <edenyefet@gmail.com>
2018-06-17 13:37:33 +0300

    Second commit

---
+++ ImageServiceAndroid/app/src/main/java/com/potatos/two/imageserviceandroid/ImageService.java
@@ -0,0 +1,86 @@
+package com.potatos.two.imageserviceandroid;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
+
+public class ImageService extends Service {
+
+    private TcpClient mTcpClient;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    public void onCreate() {
+        super.onCreate();
+
+        mTcpClient = new TcpClient();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flag, int startId) {
+
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.wifi.supplicant.CONNECTION_CHANGE");
+        intentFilter.addAction("android.net.wifi.STATE_CHANGE");
+        BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                context.getSystemService(Context.WIFI_SERVICE);
+                NetworkInfo networkInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+
+                final NotificationManager notificationManager = (NotificationManager) context.getSystemService
+                        (Context.NOTIFICATION_SERVICE);
+                NotificationChannel channel = new NotificationChannel("default", "Channel name", NotificationManager
+                        .IMPORTANCE_DEFAULT);
+                channel.setDescription("Channel description");
+
+                if (notificationManager != null) {
+                    notificationManager.createNotificationChannel(channel);
+                }
+
+                final NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default");
+                builder.setSmallIcon(R.drawable.ic_launcher_background);
+                builder.setContentTitle("Transferring Images status");
+                builder.setContentText("In progress");
+
+
+                if (networkInfo != null) {
+                    if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                        if (networkInfo.getState() == NetworkInfo.State.CONNECTED) {
+                            Toast.makeText(context, "Connected to WiFi", Toast.LENGTH_SHORT).show();
+
+                            mTcpClient.connect(notificationManager, builder);
+                        }
+                    }
+                }
+            }
+        };
+
+        registerReceiver(broadcastReceiver, intentFilter);
+        Toast.makeText(this, "Service starting...", Toast.LENGTH_SHORT).show();
+        return START_STICKY;
+    }
+
+    public void onDestroy() {
+        Toast.makeText(this, "Service stopping...", Toast.LENGTH_SHORT).show();
+//        tcpConnection.closeConnection();
+    }
+}
--- ImageServiceAndroid/app/src/main/java/com/potatos/two/imageserviceandroid/MainActivity.java
+++ ImageServiceAndroid/app/src/main/java/com/potatos/two/imageserviceandroid/MainActivity.java
@@ -6,11 +6,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.Image;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.NetworkRequest;
 import android.net.wifi.WifiManager;
 import android.os.Build;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -19,12 +22,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import static android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET;
-
 public class MainActivity extends AppCompatActivity {
 
-    private BroadcastReceiver mBroadcastReceiver;
-    private TcpClient mTcpClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,61 +40,38 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 startServiceButton.setEnabled(false);
                 stopServiceButton.setEnabled(true);
+                startService(view);
+
                 CharSequence text = "Service started...";
                 int duration = Toast.LENGTH_SHORT;
                 Toast toast = Toast.makeText(context, text, duration);
                 toast.show();
             }
         });
+
         stopServiceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startServiceButton.setEnabled(true);
                 stopServiceButton.setEnabled(false);
+                stopService(view);
+
                 CharSequence text = "Service stopped";
                 int duration = Toast.LENGTH_SHORT;
                 Toast toast = Toast.makeText(context, text, duration);
                 toast.show();
             }
         });
+    }
 
-        final IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction("android.net.wifi.supplicant.CONNECTION_CHANGE");
-        intentFilter.addAction("android.net.wifi.STATE_CHANGE");
+    private void startService(View view) {
+        Intent intent = new Intent(this, ImageService.class);
+        startService(intent);
+    }
 
-        NetworkRequest.Builder request = new NetworkRequest.Builder();
-        request.addCapability(NET_CAPABILITY_INTERNET);
-
-        mBroadcastReceiver = new BroadcastReceiver() {
-            @RequiresApi(api = Build.VERSION_CODES.O)
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-                NetworkInfo networkInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
-
-                final NotificationManager notificationManager = (NotificationManager) context.getSystemService
-                        (Context.NOTIFICATION_SERVICE);
-                NotificationChannel channel = new NotificationChannel("default", "Channelname",
-                        NotificationManager.IMPORTANCE_DEFAULT);
-                channel.setDescription("Channel description");
-                if (notificationManager != null) {
-                    notificationManager.createNotificationChannel(channel);
-                }
-                final NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default");
-                builder.setSmallIcon(R.drawable.ic_launcher_background);
-                builder.setContentTitle("Transferring Images status");
-                builder.setContentText("In progress");
-
-
-                if (networkInfo != null) {
-                    if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
-                        if (networkInfo.getState() == NetworkInfo.State.CONNECTED) {
-                            mTcpClient.connect(notificationManager, builder);
-                        }
-                    }
-                }
-            }
-        };
-
+    private void stopService(View view) {
+        Intent intent = new Intent(this, ImageService.class);
+        stopService(intent);
     }
 }
+
